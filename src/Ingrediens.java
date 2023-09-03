87ce56e8f96e6a58f119b209bd6d236519bcfcc1
Lars <tolanatolana@gmail.com>
2023-09-03 20:41:09 +0200

    Initial commit

---
+++ src/Ingrediens.java
@@ -0,0 +1,77 @@
+public class Ingrediens {
+    private String type;
+    private double mængde;
+    private String enhed;
+    private int oprindeligAntal;
+    private double kcal;
+    private double vægtPrEnhed;
+    private int antalPersoner;
+
+
+    public Ingrediens(String type, double mængde, String enhed, int oprindeligAntal, double kcal) {
+        this.type = type;
+        this.mængde = mængde;
+        this.enhed = enhed;
+        this.oprindeligAntal = oprindeligAntal;
+        this.kcal = kcal;
+        antalPersoner = oprindeligAntal;
+        vægtPrEnhed = 1;
+    }
+
+    public Ingrediens(String type, double mængde, String enhed, int oprindeligAntal, double kcal, double vægtPrEnhed) {
+        this.type = type;
+        this.mængde = mængde;
+        this.enhed = enhed;
+        this.oprindeligAntal = oprindeligAntal;
+        this.kcal = kcal;
+        this.vægtPrEnhed = vægtPrEnhed;
+        antalPersoner = oprindeligAntal;
+    }
+
+    public double beregnMængde(int antalPersoner) {
+        double faktor = (double) antalPersoner / oprindeligAntal;
+        return mængde * faktor;
+    }
+    
+    public double beregnMængde() {
+        double faktor = (double) antalPersoner / oprindeligAntal;
+        return mængde * faktor;
+    }
+
+    public double beregnVægt(int antalPersoner) {
+        double mængde = beregnMængde(antalPersoner);
+        return mængde * vægtPrEnhed;
+    }
+
+    public double beregnEnergi(int antalPersoner) {
+        double faktor = (double) antalPersoner / oprindeligAntal;
+        return faktor * kcal;
+        // 200 g mel, kcal = 50, 2 personer
+        // 4 -   4 / 2 = 2, 2 * 50
+        // 400/200 = 2 * kcal 50
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getMængde() {
+        return mængde;
+    }
+
+    public String getEnhed() {
+        return enhed;
+    }
+
+    public int getOprindeligAntal() {
+        return oprindeligAntal;
+    }
+
+    public double getKcal() {
+        return kcal;
+    }
+
+    public double getVægtPrEnhed() {
+        return vægtPrEnhed;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        final int ANTAL_PERSONER = 2;
+        System.out.println("Hello world!");
+        Ingrediens ingrediens1 = new Ingrediens("Æg", 2, "Styk",ANTAL_PERSONER,108,50);
+        Ingrediens ingrediens2 = new Ingrediens("Mel", 400, "gram",ANTAL_PERSONER,1456);
+        Ingrediens ingrediens3 = new Ingrediens("Vand", 500, "milliliter",ANTAL_PERSONER,0);
+        Ingrediens ingrediens4 = new Ingrediens("Salt", 2, "tsk.",ANTAL_PERSONER,0,10);
+        ArrayList<Ingrediens> ingrediensList = new ArrayList<>();
+        ingrediensList.add(ingrediens1);
+        ingrediensList.add(ingrediens2);
+        ingrediensList.add(ingrediens3);
+        ingrediensList.add(ingrediens4);
+
+        for (Ingrediens ing : ingrediensList) {
+            ing.antalPersoner(4);
+            System.out.println(ing.getMængde() + " "
+                    + ing.getEnhed()
+                    + " " +ing.getType());
+        }
+    }
+}

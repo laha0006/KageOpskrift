d23ddb6399f3087212c07eefce3bfb5ba1b5f1a1
Lars Friis Hansen <tolanatolana@gmail.com>
2023-09-05 20:08:28 +0200

    homework

--- src/Main.java
+++ src/Main.java
@@ -6,41 +6,35 @@ public class Main {
         final int NYT_ANTAL_PERSONER = 2;
 
         System.out.println("Kage? KAAAAAGGGEEEEEEEE!");
-
+        /*
+        instansiere ingrediens objekter.
+        Med de argumenter vi har defineret i Ingrediens klassens konstruktør
+        Type, Mændge, Enhed,oprindeligeAntalPersoner,kcal,vægtPrEnhed
+        Konstruktøren er “overloaded” dvs. Vi har 2 konstruktøren, en som ikke
+        Tager vægtPrEnhed, så hvis det sidste argument undlades, så bliver vægtPrEnehed = 1
+        */
         Ingrediens ingrediens1 = new Ingrediens("Æg", 2, "Styk", ANTAL_PERSONER, 108, 50);
         Ingrediens ingrediens2 = new Ingrediens("Mel", 400, "gram", ANTAL_PERSONER, 1456);
         Ingrediens ingrediens3 = new Ingrediens("Vand", 500, "milliliter", ANTAL_PERSONER, 0);
         Ingrediens ingrediens4 = new Ingrediens("Salt", 2, "tsk.", ANTAL_PERSONER, 0, 10);
 
+        //en liste af typen Ingrediens (den klasse vi har defineret)
+        //bliver sat = {ingrediens1,...} altså vi sætter listen til at være lig med
+        //vores 4 ingrediens objekter.
         Ingrediens[] ingrediensList = {ingrediens1, ingrediens2, ingrediens3, ingrediens4};
-        Opskrift opskrift = new Opskrift("Dej", ingrediensList);
-        System.out.println("opskrift.beregnTotalVægt() = " + opskrift.beregnTotalVægt());
-        opskrift.setAntal(4);
-        System.out.println("opskrift.beregnTotalVægt() = " + opskrift.beregnTotalVægt());
 
-        double totalVægt = 0;
-        double totalKcal = 0;
-        double totalKjoule = 0; // 4
-        int count = ingrediensList.length;
-        for (Ingrediens ing : ingrediensList) {
-            ing.setAntal(NYT_ANTAL_PERSONER);
-            System.out.println(ing.beregnMængde() + " "
-                    + ing.getEnhed()
-                    + " " + ing.getType()
-                    + " vægt: " + ing.beregnVægt()
-                    + " kcal: " + ing.beregnKcal()
-                    + " kjoule: " + ing.beregnKjoule());
-            totalVægt += ing.beregnVægt(); // totalVægt = totalVægt + ing.beregnVægt();
-            totalKcal += ing.beregnKcal();
-            totalKjoule += ing.beregnKjoule();
-        }
-        System.out.println();
-        System.out.println("totalVægt = " + totalVægt);
-        System.out.println("totalKcal = " + totalKcal);
-        System.out.println("totalKjoule = " + totalKjoule);
-        System.out.println("Gennemsnitlig Vægt: " + totalVægt / count);
-        System.out.println("Gennemsnitlig kcal: " + totalKcal / count);
-        System.out.println("Gennemsnitlig kjoule: " + totalKjoule / count);
-        System.out.println();
+        //instansiere et opskrift objekt, som vi giver 2 arugmenter
+        //Først et navn til opskriftet, og derefter den liste med ingredienser
+        //vi har defineret for oven.
+        Opskrift opskrift = new Opskrift("Dej", ingrediensList);
+
+        //print opskrift data ud.
+        System.out.println("\nTil 2 personer: ");
+        opskrift.printOpskrift();
+
+        //sæt et nyt antal personer og print igen.
+        opskrift.setAntal(4);
+        System.out.println("\nTil 4 Personer: ");
+        opskrift.printOpskrift();
     }
 }
--- src/Opskrift.java
+++ src/Opskrift.java
@@ -1,12 +1,22 @@
 public class Opskrift {
-    private String name;
-    private Ingrediens[] ingrediensList;
+    private final String name;
+    private final Ingrediens[] ingrediensList;
+    private int antalPersoner;
 
-    public Opskrift(String navn, Ingrediens[] ingrediensList) {
-        this.name = navn;
+
+    //Opskrift konstruktør, med 2 parametre, name, og en liste af ingredienser
+    //dvs. når man instansiere et opskrift objekt, skal man give de 2 argumenter
+    //et navn, og en liste af ingredienser.
+    public Opskrift(String name, Ingrediens[] ingrediensList) {
+        this.name = name;
         this.ingrediensList = ingrediensList;
     }
 
+    //metoden bruger objektets liste af ingredienser
+    //som er sat når man har instansieret et nyt opskrift objekt.
+    //metoden bruger en for each loop til at gå gennem hver ingrediens
+    //i listen og ligge det til det lokale variable totalVægt
+    //når loopen er færdig retuneres det lokale variable totalVægt.
     public double beregnTotalVægt() {
         double totalVægt = 0;
         for (Ingrediens i : ingrediensList) {
@@ -19,21 +29,45 @@ public class Opskrift {
         double totalKcal = 0;
         for (Ingrediens i : ingrediensList) {
             totalKcal += i.beregnKcal();
-
         }
         return totalKcal;
     }
+    public double beregnTotalKjoule() {
+        double totalKjoule = 0;
+        for (Ingrediens i : ingrediensList) {
+            totalKjoule += i.beregnKjoule();
+        }
+        return totalKjoule;
+    }
 
+    //metoend kalder BeregnTotalVægt() for at få den totale vægt
+    //så man ikke behøver at lave den samme kode/udregning igen.
+    //derefter bliver det divideret med ingredienslistens længde.
+    //resultatet af udregningen bliver retuneret.
+    public double gennemsnitVægt(){
+        return beregnTotalVægt() / ingrediensList.length;
+    }
+    public double gennemsnitKcal(){
+        return beregnTotalKcal() / ingrediensList.length;
+    }
+    public double gennemsnitKjoule(){
+        return beregnTotalKjoule() / ingrediensList.length;
+    }
+
+    //dette er en set metode, til at sætte antallet af personer
+    //som opskriften skal beregne ud fra.
+    //den looper igennem ingrediens listen og sætter hver ingrediens
+    //til antalPersoner argumentets værdi
     public void setAntal(int antalPersoner) {
+        this.antalPersoner = antalPersoner; // bliver ikke brugt endnu, til fremtidig brug af udregning af gennemsnit per person.
         for (Ingrediens i : ingrediensList) {
             i.setAntal(antalPersoner);
         }
     }
 
-
-    // beregnTotalKjoule
-    // gennemsnitVægt, Kcal, Kjoule
-    // setAntal -- For loop og sæt antalPersoner
+    //print metoden looper igen igennem hver ingrediens
+    //og printer data ud
+    //til sidst kalder vi opskrifts metoder til at vise totaler, og gennemsnit.
     public void printOpskrift() {
         for (Ingrediens ing : ingrediensList) {
             System.out.println(ing.beregnMængde() + " "
@@ -44,6 +78,12 @@ public class Opskrift {
                     + " kjoule: " + ing.beregnKjoule());
         }
         System.out.println("Total Vægt: " + beregnTotalVægt());
+        System.out.println("Total Kcal: " + beregnTotalKcal());
+        System.out.println("Total Kjoule: " + beregnTotalKjoule());
+        System.out.println("Gennemsnits Vægt: " + gennemsnitVægt());
+        System.out.println("Gennemsnits Kcal: " + gennemsnitKcal());
+        System.out.println("Gennemsnits Kjoule: " + gennemsnitKjoule());
+
     }
 }
 
